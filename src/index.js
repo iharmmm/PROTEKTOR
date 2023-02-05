0a37d5976ca273d926e4292deba67067b39d630a
Renthop <lapygin.03@mail.ru>
2023-02-05 17:00:55 +0300

    добавил кнопку на страницу

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './index.html';
 import './index.scss';
 import './modules/swiper-bundle.min.js';
 import './modules/swiper.js';
+import './modules/buttonToUp.js';
 import './modules/calc.js';
 import './modules/modal.js';
